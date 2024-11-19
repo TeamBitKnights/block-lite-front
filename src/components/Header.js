e21228bc9ddffc882fcc65e865c2b7c530519543
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-11-19 09:48:32 +0900

    del : leo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
-import React, { useEffect, useState } from "react";
-import "./Header.css";
-import { Generation_logo } from "../images/_index";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import './Header.css';
+import { Generation_logo } from '../images/_index';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { Link, useParams } from 'react-router-dom';
 
 const Header = () => {
   const [toggleOn, setToggleOn] = useState(false);
@@ -41,30 +41,24 @@ const Header = () => {
       </div>
       <ul className="navBar">
         <li>
-          <Link to="/" style={{ textDecoration: "none" }}>
+          <Link to="/" style={{ textDecoration: 'none' }}>
             <h1>Home</h1>
           </Link>
         </li>
         <li>
-          <Link to="/blocks" style={{ textDecoration: "none" }}>
+          <Link to="/blocks" style={{ textDecoration: 'none' }}>
             <h1>Blocks</h1>
           </Link>
         </li>
         <li>
-          <Link to="/txs" style={{ textDecoration: "none" }}>
+          <Link to="/txs" style={{ textDecoration: 'none' }}>
             <h1>Txs</h1>
           </Link>
         </li>
         <li>
-          <Link to="/charts" style={{ textDecoration: "none" }}>
+          <Link to="/charts" style={{ textDecoration: 'none' }}>
             <h1>Charts</h1>
           </Link>
-        </li>
-        <li>
-          <h2>|</h2>
-        </li>
-        <li>
-          <h3>Leo</h3>
         </li>
       </ul>
 
@@ -72,29 +66,28 @@ const Header = () => {
       {toggleOn ? (
         <ul className="navBar_On">
           <li onClick={() => toggleBtnOff()}>
-            <Link to="/" style={{ textDecoration: "none" }}>
+            <Link to="/" style={{ textDecoration: 'none' }}>
               <h1>Home</h1>
             </Link>
           </li>
           <li onClick={() => toggleBtnOff()}>
-            <Link to="/blocks" style={{ textDecoration: "none" }}>
+            <Link to="/blocks" style={{ textDecoration: 'none' }}>
               <h1>Blocks</h1>
             </Link>
           </li>
           <li onClick={() => toggleBtnOff()}>
-            <Link to="/txs" style={{ textDecoration: "none" }}>
+            <Link to="/txs" style={{ textDecoration: 'none' }}>
               <h1>Txs</h1>
             </Link>
           </li>
           <li onClick={() => toggleBtnOff()}>
-            <Link to="/charts" style={{ textDecoration: "none" }}>
+            <Link to="/charts" style={{ textDecoration: 'none' }}>
               <h1>Charts</h1>
             </Link>
           </li>
         </ul>
       ) : null}
       <div className="navBar_toggle" onClick={() => toggleBtn()}>
-        <h3 className="network">Leo</h3>
         <GiHamburgerMenu size={30} />
       </div>
     </header>
